f3386e7dbdf4239d2540cb1333e41513157781a6
Seth Burns <seth@sethburns.com>
2023-04-20 13:10:11 -0700

    Refactor HTML advanced and classic to one gameboard

--- main.js
+++ main.js
@@ -4,69 +4,98 @@ var game = {
 };
 
 // Query Selectors //
-var playerInfo = document.querySelector(".player-1");
-var computerInfo = document.querySelector(".computer-player");
-var classicChoice = document.querySelector(".classic");
-var advancedChoice = document.querySelector(".advanced");
-var gameArea = document.querySelector(".game-area");
-var playerAvatar = document.querySelector(".player-1");
-var selectGame = document.querySelector(".screen-title");
-var classicGame = document.querySelector(".classic-game");
-var advancedGame = document.querySelector(".advanced-game");
-var chooseWeapon = document.querySelector(".choose-your-weapon");
-var choiceDisplay = document.querySelector(".choice-display");
-var changeGame = document.querySelector(".change-game");
+var playerInfo = document.querySelector('.player-1');
+var computerInfo = document.querySelector('.computer-player');
+var classicChoice = document.querySelector('.classic');
+var advancedChoice = document.querySelector('.advanced');
+var gameArea = document.querySelector('.game-area');
+var playerAvatar = document.querySelector('.player-1');
+var selectGame = document.querySelector('.screen-title');
+var gameBoard = document.querySelector('.game-board');
+var chooseWeapon = document.querySelector('.choose-your-weapon');
+var choiceDisplay = document.querySelector('.choice-display');
+var changeGame = document.querySelector('.change-game');
 
 // Event Listeners //
-window.addEventListener("load", function () {
-  createPlayer("Human", "./assets/straightface.png");
-  createPlayer("Computer", "./assets/computer.png");
-  playerInfo.innerHTML = `<img src='${game.players[0].avatar}' alt="Player Avatar" class="avatar" />
+window.addEventListener('load', function () {
+  createPlayer('Human', './assets/straightface.png');
+  createPlayer('Computer', './assets/computer.png');
+  playerInfo.innerHTML = `
+  <img src='${game.players[0].avatar}' alt="Player Avatar" class="avatar" />
   <h2>${game.players[0].name}</h2>
   <p class="player-wins">Wins: ${game.players[0].wins}</p>`;
-  computerInfo.innerHTML = `<img src='${game.players[1].avatar}' alt="Player Avatar" class="avatar" />
+  computerInfo.innerHTML = `
+  <img src='${game.players[1].avatar}' alt="Player Avatar" class="avatar" />
   <h2>${game.players[1].name}</h2>
   <p class="player-wins">Wins: ${game.players[1].wins}</p>`;
-  console.log(game);
 });
 
-classicChoice.addEventListener("click", function () {
+classicChoice.addEventListener('click', function () {
+  createGame('classic');
+  gameBoard.innerHTML = `<section class="classic-game">
+    <img src="./assets/boulder.png" alt="Big Boulder" class="boulder hover" />
+    <img src="./assets/parchment.png" alt="Parchment Paper" class="parchment hover" />
+    <img src="./assets/shears.png" alt="Cutting Shears" class="shears hover" />
+  </section>`;
   hide(advancedChoice);
   hide(classicChoice);
   hide(selectGame);
   show(chooseWeapon);
-  show(classicGame);
+  show(gameBoard);
   show(choiceDisplay);
   show(changeGame);
-  createGame("classic");
-  console.log(game);
 });
 
-advancedChoice.addEventListener("click", function () {
+advancedChoice.addEventListener('click', function () {
+  createGame('advanced');
+  gameBoard.innerHTML = `<section class="advanced-game">
+    <img src="./assets/boulder.png" alt="Big Boulder" class="boulder hover" />
+    <img src="./assets/parchment.png" alt="Parchment Paper" class="parchment hover" />
+    <img src="./assets/shears.png" alt="Cutting Shears" class="shears hover" />
+    <img src="./assets/dragon.png" alt="Dragon" class="dragon hover" />
+    <img src="./assets/wizard.png" alt="Wizard" class="wizard hover" />
+  </section>`;
   hide(advancedChoice);
   hide(classicChoice);
   hide(selectGame);
   show(chooseWeapon);
-  show(advancedGame);
+  show(gameBoard);
   show(choiceDisplay);
   show(changeGame);
-  createGame("advanced");
 });
 
-classicGame.addEventListener("click", function (event) {
-  if (event.target.classList.contains("boulder")) {
-    console.log("clicked boulder");
+changeGame.addEventListener('click', function () {
+  hide(gameBoard);
+  hide(chooseWeapon);
+  hide(choiceDisplay);
+  hide(changeGame);
+  show(advancedChoice);
+  show(classicChoice);
+  show(selectGame);
+});
+
+gameBoard.addEventListener('click', function (event) {
+  var playerChoice;
+  if (event.target.classList.contains('boulder')) {
+    console.log('boulder');
+    playerChoice = 'boulder';
+  }
+  if (event.target.classList.contains('parchment')) {
+    playerChoice = 'parchment';
+  }
+  if (event.target.classList.contains('shears')) {
+    playerChoice = 'shears';
   }
 });
 
 // Conjunction Junction, What's Your... //
 
 function hide(element) {
-  element.classList.add("hidden");
+  element.classList.add('hidden');
 }
 
 function show(element) {
-  element.classList.remove("hidden");
+  element.classList.remove('hidden');
 }
 
 function createPlayer(name, token) {
@@ -79,10 +108,10 @@ function createPlayer(name, token) {
 
 function createGame(gameChoice) {
   game.gameChoice = gameChoice;
-  if (game.gameChoice === "classic") {
-    game.options = ["shears", "parchment", "boulder"];
-  } else if (game.gameChoice === "advanced") {
-    game.options = ["boulder", "shears", "dragon", "parchment", "wizard"];
+  if (game.gameChoice === 'classic') {
+    game.options = ['shears', 'parchment', 'boulder'];
+  } else if (game.gameChoice === 'advanced') {
+    game.options = ['boulder', 'shears', 'dragon', 'parchment', 'wizard'];
   }
 }
 
