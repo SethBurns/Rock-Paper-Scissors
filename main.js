7124d632065541929c85851264847995aca40f90
Seth Burns <seth@sethburns.com>
2023-04-24 13:59:29 -0700

    Make sounds and mute button work, change shears to sword.

--- main.js
+++ main.js
@@ -4,55 +4,43 @@ var game = {
 };
 
 // Query Selectors //
-var playerInfo = document.querySelector('.player-1');
-var computerInfo = document.querySelector('.computer-player');
-var classicChoice = document.querySelector('.classic');
 var advancedChoice = document.querySelector('.advanced');
-var gameArea = document.querySelector('.game-area');
-var selectGame = document.querySelector('.screen-title');
-var gameBoard = document.querySelector('.game-board');
 var chooseWeapon = document.querySelector('.choose-your-weapon');
-var choiceDisplay = document.querySelector('.choice-display');
 var changeGame = document.querySelector('.change-game');
-var gameResult = document.querySelector('.game-result');
+var choiceDisplay = document.querySelector('.choice-display');
+var classicChoice = document.querySelector('.classic');
+var computerInfo = document.querySelector('.computer-player');
 var choices = document.querySelector('.current-match');
+var gameArea = document.querySelector('.game-area');
+var gameBoard = document.querySelector('.game-board');
+var gameResult = document.querySelector('.game-result');
+var soundButton = document.querySelector('.mute');
+var playerInfo = document.querySelector('.player-1');
+var selectGame = document.querySelector('.screen-title');
+
+var dragon = new Audio('./assets/dragonroar.mp3');
+var paper = new Audio('./assets/paper.mp3');
+var rock = new Audio('./assets/rock.mp3');
+var sword = new Audio('./assets/sword.mp3');
+var wizard = new Audio('./assets/wizardsound.mp3');
 
 // Event Listeners //
 window.addEventListener('load', function () {
   createPlayer('Human', './assets/straightface.png');
   createPlayer('Computer', './assets/computer.png');
-  playerInfo.innerHTML = `
-  <img src='${game.players[0].avatar}' alt="Player Avatar" class="avatar" />
-  <h2>${game.players[0].name}</h2>
-  <p class="player-wins">Wins: ${game.players[0].wins}</p>`;
-  computerInfo.innerHTML = `
-  <img src='${game.players[1].avatar}' alt="Player Avatar" class="avatar" />
-  <h2>${game.players[1].name}</h2>
-  <p class="player-wins">Wins: ${game.players[1].wins}</p>`;
+  updatePlayerDom();
 });
 
 classicChoice.addEventListener('click', function () {
   createGame('classic');
-  setClassicGame();
-  hide(advancedChoice);
-  hide(classicChoice);
-  hide(selectGame);
-  show(chooseWeapon);
-  show(gameBoard);
-  show(choiceDisplay);
-  show(changeGame);
+  setClassicGameDom();
+  prepareGameBoard();
 });
 
 advancedChoice.addEventListener('click', function () {
   createGame('advanced');
-  setAdvancedGame();
-  hide(advancedChoice);
-  hide(classicChoice);
-  hide(selectGame);
-  show(chooseWeapon);
-  show(gameBoard);
-  show(choiceDisplay);
-  show(changeGame);
+  setAdvancedGameDom();
+  prepareGameBoard();
 });
 
 changeGame.addEventListener('click', function () {
@@ -72,13 +60,38 @@ gameBoard.addEventListener('click', function (event) {
     }
   }
   takeTurn(game.playerChoice);
-  updatePlayers();
+  updatePlayersDom();
+  playAudio();
   hide(gameBoard);
   hide(changeGame);
-  setTimeout(resetGameBoard, 5000);
+  setTimeout(resetGameBoard, 4000);
+});
+
+soundButton.addEventListener('click', function () {
+  soundToggle();
 });
 
 // Conjunction Junction, What's Your... //
+function updatePlayerDom() {
+  playerInfo.innerHTML = `
+  <img src='${game.players[0].avatar}' alt="Player Avatar" class="avatar" />
+  <h2>${game.players[0].name}</h2>
+  <p class="player-wins">Wins: ${game.players[0].wins}</p>`;
+  computerInfo.innerHTML = `
+  <img src='${game.players[1].avatar}' alt="Player Avatar" class="avatar" />
+  <h2>${game.players[1].name}</h2>
+  <p class="player-wins">Wins: ${game.players[1].wins}</p>`;
+}
+
+function prepareGameBoard() {
+  hide(advancedChoice);
+  hide(classicChoice);
+  hide(selectGame);
+  show(chooseWeapon);
+  show(gameBoard);
+  show(choiceDisplay);
+  show(changeGame);
+}
 
 function hide(element) {
   element.classList.add('hidden');
@@ -106,9 +119,9 @@ function createPlayer(name, token) {
 function createGame(gameChoice) {
   game.gameChoice = gameChoice;
   if (game.gameChoice === 'classic') {
-    game.options = ['shears', 'parchment', 'boulder'];
+    game.options = ['sword', 'parchment', 'boulder'];
   } else if (game.gameChoice === 'advanced') {
-    game.options = ['boulder', 'shears', 'dragon', 'parchment', 'wizard'];
+    game.options = ['boulder', 'sword', 'dragon', 'parchment', 'wizard'];
   }
 }
 
@@ -119,21 +132,29 @@ function computerChoice() {
 
 function takeTurn(playerChoice) {
   game.compChoice = computerChoice();
-  // game.playerChoice = playerChoice;
-  choices.innerHTML = `<img src="./assets/${game.playerChoice}.png" class="${game.playerChoice}" />
-  <img src="./assets/vs.png">
-  <img src="./assets/${game.compChoice}.png" class="${game.compChoice}" />`;
+  displayChoicesDom();
   if (checkForDraw(playerChoice, game.compChoice)) {
-    gameResult.innerText = `IT'S A TIE!`;
+    updateTieDom();
   } else {
     return checkWhoWon(playerChoice, game.compChoice);
   }
+}
+
+function updateTieDom() {
+  gameResult.innerText = `IT'S A TIE!`;
+}
+
+function displayChoicesDom() {
+  choices.innerHTML = `<img src="./assets/${game.playerChoice}.png" class="${game.playerChoice}" />
+  <img src="./assets/vs.png">
+  <img src="./assets/${game.compChoice}.png" class="${game.compChoice}" />`;
 }
 
 function checkForDraw(playerChoice, computerChoice) {
   return playerChoice === computerChoice;
 }
 
+// Explain this function/maybe refactor//
 function checkWhoWon(playerChoice, computerChoice) {
   var playerIndex = game.options.indexOf(playerChoice);
   var computerIndex = game.options.indexOf(computerChoice);
@@ -143,14 +164,24 @@ function checkWhoWon(playerChoice, computerChoice) {
     (playerIndex > computerIndex && playerIndex > computerIndex + decider)
   ) {
     game.players[0].wins++;
-    gameResult.innerText = `YOU WIN! ${playerChoice} beats ${computerChoice}!`;
+    game.currentWinner = playerChoice;
+    updateWinnerDom(playerChoice, computerChoice);
   } else {
     game.players[1].wins++;
-    gameResult.innerText = `You lost... ${computerChoice} beats ${playerChoice}.`;
+    game.currentWinner = computerChoice;
+    updateLoserDom(computerChoice, playerChoice);
   }
 }
 
-function updatePlayers() {
+function updateWinnerDom(playerChoice, computerChoice) {
+  gameResult.innerText = `YOU WIN... ${playerChoice} beats ${computerChoice}!`;
+}
+
+function updateLoserDom(computerChoice, playerChoice) {
+  gameResult.innerText = `You lost... ${computerChoice} beats ${playerChoice}.`;
+}
+
+function updatePlayersDom() {
   if (game.players[0].wins === game.players[1].wins) {
     game.players[0].avatar = './assets/straightface.png';
   } else if (game.players[0].wins < game.players[1].wins) {
@@ -158,6 +189,7 @@ function updatePlayers() {
   } else {
     game.players[0].avatar = './assets/smileyface.png';
   }
+
   playerInfo.innerHTML = `
   <img src='${game.players[0].avatar}' alt="Player Avatar" class="avatar" />
   <h2>${game.players[0].name}</h2>
@@ -168,20 +200,65 @@ function updatePlayers() {
   <p class="player-wins">Wins: ${game.players[1].wins}</p>`;
 }
 
-function setAdvancedGame() {
-  gameBoard.innerHTML = `<section class="advanced-game">
+function setAdvancedGameDom() {
+  gameBoard.innerHTML = `
+  <section class="advanced-choice-display">
+    <section class="advanced-game">
     <img src="./assets/boulder.png" alt="Big Boulder" class="boulder hover" />
     <img src="./assets/parchment.png" alt="Parchment Paper" class="parchment hover" />
-    <img src="./assets/shears.png" alt="Cutting Shears" class="shears hover" />
+    <img src="./assets/sword.png" alt="Cutting sword" class="sword hover" />
+    </section>
+    <section class="advanced-game">
     <img src="./assets/dragon.png" alt="Dragon" class="dragon hover" />
     <img src="./assets/wizard.png" alt="Wizard" class="wizard hover" />
+    </section>
   </section>`;
 }
 
-function setClassicGame() {
+function setClassicGameDom() {
   gameBoard.innerHTML = `<section class="classic-game">
     <img src="./assets/boulder.png" alt="Big Boulder" class="boulder hover" />
     <img src="./assets/parchment.png" alt="Parchment Paper" class="parchment hover" />
-    <img src="./assets/shears.png" alt="Cutting Shears" class="shears hover" />
+    <img src="./assets/sword.png" alt="Cutting sword" class="sword hover" />
     </section>`;
+}
+
+function soundToggle() {
+  if (game.sound === 'muted') {
+    updateSoundDom();
+    game.sound = 'sound on';
+  } else {
+    updateSoundDom();
+    game.sound = 'muted';
+  }
+}
+
+function updateSoundDom() {
+  if (soundButton.innerText === 'SOUND ON') {
+    soundButton.innerText = 'MUTED';
+  } else {
+    soundButton.innerText = 'SOUND ON';
+  }
+}
+
+function playAudio() {
+  if (game.playerChoice === game.compChoice || game.sound === 'muted') {
+    return;
+  }
+  if (game.currentWinner === 'sword') {
+    sword.volume = 0.2;
+    sword.play();
+  } else if (game.currentWinner === 'parchment') {
+    paper.volume = 0.1;
+    paper.play();
+  } else if (game.currentWinner === 'boulder') {
+    rock.volume = 0.1;
+    rock.play();
+  } else if (game.currentWinner === 'dragon') {
+    dragon.volume = 0.15;
+    dragon.play();
+  } else {
+    wizard.volume = 0.1;
+    wizard.play();
+  }
 }
